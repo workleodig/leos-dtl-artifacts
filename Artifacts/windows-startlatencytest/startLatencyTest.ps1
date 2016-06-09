519196cd8c6fdc0680230c849268613f5ecf5d37
Leo Dignan <ldignan@microsoft.com>
2016-06-09 16:51:57 -0500

    Adding latency artifact

---
+++ Artifacts/windows-startlatencytest/startLatencyTest.ps1
@@ -0,0 +1,17 @@
+﻿$SQLServer = "dtlloadtestserver.database.windows.net"
+$SQLDBName = "DTLLoadTestDB "
+$uid ="ldignan"
+$pwd = "adjf@CMK9vldf9a"
+$machineName  = $env:COMPUTERNAME
+$SqlQuery = "exec RegisterListener '$machineName'"
+
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName; User ID = $uid; Password = $pwd;"
+$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+$SqlCmd.CommandText = $SqlQuery
+$SqlCmd.Connection = $SqlConnection
+$SqlCmd.Connection.Open();
+$SqlCmd.ExecuteNonQuery();
+
+
+
